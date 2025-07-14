1fccd13b1a1f993c8018c43b062d2367cf36a53c
cx-029 <htt99113@163.com>
2025-07-14 14:50:14 +0800

    测试分支修改

--- src/main/java/com/mybank/backend/MyBankBackendApplication.java
+++ src/main/java/com/mybank/backend/MyBankBackendApplication.java
@@ -3,6 +3,8 @@ package com.mybank.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//测试分支修改
+
 @SpringBootApplication
 public class MyBankBackendApplication {
 
